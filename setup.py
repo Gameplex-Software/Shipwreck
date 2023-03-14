11da19a2c5970bd28d31d08be5db49226fec1b42
Tristan Poland <34868944+tristanpoland@users.noreply.github.com>
2023-03-14 17:14:58 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,36 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='Shipwreck',
-    version='0.1.3',
-    description='A library for logging and handling Python errors and suggesting fixes',
-    author='Gameplex Sfotware',
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ShipWreck',
+    author='Gameplex Software',
     author_email='info@gameplexsoftware.com',
-    install_requires=[
-        # Add any dependencies required by the library.
-    ],
+    description='A library for logging and handling Python errors and suggesting fixes',
+    keywords='ShipWreck, pypi, package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/gameplex-software/ShipWreck',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
+
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Build Tools',
+
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
+    python_requires='>=3.8',
+    extras_require={
+        'dev': ['check-manifest'],
+    },
 )
